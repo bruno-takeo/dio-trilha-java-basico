1e3f34a5836bd3662e78ff72c03a5bd2cfe00794
BrunoTakeo <bruno_takeo19@hotmail.com>
2025-03-12 16:23:49 -0300

    exemplo de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        System.out.println(nomeCompleto);
+        System.out.println();
+
+        int a = 6;
+        int b = 6;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+
+    }
+
+}
