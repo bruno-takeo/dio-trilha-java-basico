354b94c8dc1a6a66375b0d0d6558dc2d387dedaa
BrunoTakeo <bruno_takeo19@hotmail.com>
2025-03-17 11:48:16 -0300

    exemplo exceções

--- estruturas-repeticao/EstruturasRepeticao/src/AboutMe.java
+++ estruturas-repeticao/EstruturasRepeticao/src/AboutMe.java
@@ -1,11 +1,12 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 
 public class AboutMe {
     public static void main(String[] args) {
-        
-        Scanner teclado = new Scanner(System.in).useLocale(Lacale.US);
+        try{
+        Scanner teclado = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome: ");
         String nome = teclado.next();
@@ -14,9 +15,24 @@ public class AboutMe {
         String sobreNome = teclado.next();
         
         System.out.println("Digite sua idade: ");
-        int idade = teclado.next();
-
+        int idade = teclado.nextInt();
         
+        System.out.println("Digite sua altura: ");
+        double altura = teclado.nextDouble();
+
+
+        System.out.println("Olá me chamo " + nome.toUpperCase() + sobreNome.toUpperCase());
+        System.out.println("Tenho " + idade + " de idade");
+        System.out.println("Minha altura é " + altura + " Metros");
+        teclado.close();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Os campos idade e altura precisam ser numericos");
+        }
+
+
+
+
 
     }
     
